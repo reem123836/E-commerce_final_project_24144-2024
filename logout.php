ef604dca794da6e81eabbea7c7ead0764b918bdd
Reem Osama <reemosamasalih@gmail.com>
2026-06-16 13:53:37 +0200

    final project submission, all features completed

--- logout.php
+++ logout.php
@@ -1,20 +1,7 @@
 <?php
-session_start();
-
-$temp_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : null;
-
-
-session_unset();
-session_destroy();
-
-
-session_start();
-
-
-if ($temp_cart !== null) {
-    $_SESSION['cart'] = $temp_cart;
-}
-
-header("Location: auth.php");
+session_start();      
+session_unset();     
+session_destroy();    
+header("Location: auth.php"); 
 exit;
 ?>
